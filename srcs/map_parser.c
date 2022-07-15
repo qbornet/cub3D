cd18026df906ba7d21ac5374d66a7020a3fe2790
Julien FRANCAIS <jfrancai@student.42.fr>
2022-07-15 07:47:20 +0200

    norme map_parser.c

--- srcs/map_parser.c
+++ srcs/map_parser.c
@@ -1,86 +1,74 @@
 #include <cub3D.h>
 
+int	check_middle_line(const char *s1, const char *s2)
+{
+	int	i;
+	int	len1;
+	int	len2;
+
+	len1 = ft_strlen(s1) - 1;
+	if (s1[0] == '0' || s1[len1] == '0')
+		return (-1);
+	len2 = ft_strlen(s2);
+	i = 1;
+	while (i < len1)
+	{
+		if (s1[i] == '0')
+		{
+			if ((s1[i + 1] && s1[i + 1] == ' ') || i >= len2)
+				return (-1);
+			if (i - 1 >= len2 || s2[i - 1] == ' ')
+				return (-1);
+			if (i >= len2 || s2[i] == ' ')
+				return (-1);
+			if (i + 1 >= len2 || s2[i + 1] == ' ')
+				return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	check_edge_line(const char *s)
+{
+	int	i;
+	int	len;
+
+	if (!s)
+		return (-1);
+	i = 0;
+	len = ft_strlen(s);
+	while (i < len)
+	{
+		if (s[i] == '0')
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
 int	check_line(char *l_bef, char *l)
 {
-	int		i;
-	int		len;
-	int		len_bef;
-
 	if (!l_bef && !l)
 		return (-1);
 	if ((l_bef && !l_bef[0]) || (l && !l[0]))
 		return (-1);
 	if (!l_bef && l)
 	{
-		len = ft_strlen(l);
-		i = 0;
-		while (i < len)
-		{	
-			if (l[i] == '0')
-				return (-1);
-			i++;
-		}
-		return (0);
+		if (check_edge_line(l) < 0)
+			return (-1);
 	}
 	else if (l_bef && !l)
 	{
-		i = 0;
-		len_bef = ft_strlen(l_bef);
-		while (i < len_bef)
-		{
-			if (l_bef[i] == '0')
-				return (-1);
-			i++;
-		}
-		return (0);
+		if (check_edge_line(l_bef) < 0)
+			return (-1);
 	}
 	else
 	{
-		len = ft_strlen(l) - 1;
-		if (l[0] == '0' || l[len] == '0')
+		if (check_middle_line(l, l_bef) < 0)
 			return (-1);
-		len_bef = ft_strlen(l_bef);
-		i = 1;
-		while (i < len)
-		{
-			if (l[i] == '0')
-			{
-				if (l[i + 1] && l[i + 1] == ' ')
-					return (-1);
-				if (i >= len_bef)
-					return (-1);
-				if (i - 1 >= len_bef || l_bef[i - 1] == ' ')
-					return (-1);
-				if (i >= len_bef || l_bef[i] == ' ')
-					return (-1);
-				if (i + 1 >= len_bef || l_bef[i + 1] == ' ')
-					return (-1);
-			}
-			i++;
-		}
-		len_bef = ft_strlen(l_bef) - 1;
-		if (l_bef[0] == '0' || l_bef[len_bef] == '0')
+		if (check_middle_line(l_bef, l) < 0)
 			return (-1);
-		len = ft_strlen(l);
-		i = 1;
-		while (i < len_bef)
-		{
-			if (l_bef[i] == '0')
-			{
-				if (l_bef[i + 1] && l_bef[i + 1] == ' ')
-					return (-1);
-				if (i >= len)
-					return (-1);
-				if (i - 1 >= len || l[i - 1] == ' ')
-					return (-1);
-				if (i >= len || l[i] == ' ')
-					return (-1);
-				if (i + 1 >= len || l[i + 1] == ' ')
-					return (-1);
-			}
-			i++;
-		}
-		return (0);
 	}
 	return (0);
 }
