63f26843c26358e2ada8b33a9d3331cd1198801f
Julien FRANCAIS <jfrancai@student.42.fr>
2022-08-15 13:50:12 +0200

    update error message

--- srcs/cub3D.c
+++ srcs/cub3D.c
@@ -74,13 +74,13 @@ int	main(int ac, char **av)
 	t_data	*frame;
 
 	if (ac > 2)
-		return (ft_retputstr_int("Error: too many arguments\n", 2, 0));
+		return (ft_retputstr_int("Error\nToo many arguments\n", 2, 0));
 	if (ac == 1)
 		return (ft_retputstr_int(\
-					"Error: need one argument <path_to_file.cub>\n", 2, 0));
+					"Error\nNeed one argument <path_to_file.cub>\n", 2, 0));
 	if (!valid_name(av[1]))
 		return (ft_retputstr_int(\
-					"Error: with name doesn't have a .cub extansion\n", 2, 0));
+					"Error\nWith name doesn't have a .cub extansion\n", 2, 0));
 	if (frame_alloc(&frame, av[1]) < 0)
 		return (free_all(&frame));
 	start_window(&frame);
